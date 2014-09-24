d6de546dd762d166ba414d6de3167bccc9835b63
johnheroy <johnheroy@gmail.com>
2014-09-24 15:56:10 -0700

    Create SongQueueView

--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -1,11 +1,21 @@
 // SongQueueView.js - Defines a backbone view class for the song queue.
 var SongQueueView = Backbone.View.extend({
 
+  tagName: 'table',
+
   initialize: function() {
+    this.render();
+    this.listenTo(this.collection, 'add', this.render);
   },
 
   render: function() {
-    return this.$el;
+    this.$el.html('<th><td>Song Queue</td></th>').append(
+      // Song Queue's Entry Views to be added here
+      this.collection.map(function(song, index){
+        return (new SongQueueEntryView({model: song}).render()).prepend($('<td>').text(index+1));
+      })
+    );
   }
 
 });
+
